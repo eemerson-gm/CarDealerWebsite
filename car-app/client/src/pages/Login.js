00c9aa571877d0b9717b240eee6e3ff247c884b3
Eric Emerson <EricEmerson@users.noreply.github.com>
2021-03-25 21:12:38 -0400

    Added simple signup and login system. Need to finish storing account session variables.

--- car-app/client/src/pages/Login.js
+++ car-app/client/src/pages/Login.js
@@ -5,17 +5,42 @@ class Login extends Component{
     state = {
         username: "",
         password: "",
+        fullname: "",
+        address: "",
+        email: "",
         response: ""
     }
 
-    handleSubmit = async e => {
+    handleSubmitLogin = async e => {
         e.preventDefault();
-        const response = await fetch('/api/world', {
+        const response = await fetch('/api/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ username: this.state.username }),
+        body: JSON.stringify({ 
+            username: this.state.username,
+            password: this.state.password
+        }),
+        });
+        const body = await response.text();
+        this.setState({ response: body });
+    }
+
+    handleSubmitSignup = async e => {
+        e.preventDefault();
+        const response = await fetch('/api/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ 
+            username: this.state.username,
+            password: this.state.password,
+            fullname: this.state.fullname,
+            address: this.state.address,
+            email: this.state.email
+        }),
         });
         const body = await response.text();
         this.setState({ response: body });
@@ -25,35 +50,104 @@ class Login extends Component{
         return (
             <>
                 <div className="flex-div">
-                    <div className="basic-div">
-                        <form onSubmit={this.handleSubmit}>
-                            <table>
-                                <tr>
-                                    <td>
-                                        <div className="flex-div">
-                                            <h2>Username:</h2>
-                                            <input id="textUsername" type="text" value={this.state.username} onChange={e => this.setState({ username: e.target.value })}></input>
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <div className="flex-div">
-                                            <h2>Password:</h2>
-                                            <input id="textPassword" type="text" value={this.state.password} onChange={e => this.setState({ password: e.target.value })}></input>
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <div className="flex-div">
-                                            <input id="buttonSubmit" type="submit" value="Login"></input>
-                                        </div>
-                                    </td>
-                                </tr>
-                            </table>
-                        </form>
-                    </div>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div className="basic-div">
+                                        <form onSubmit={this.handleSubmitSignup}>
+                                            <table>
+                                                <tr>
+                                                    <td>
+                                                        <div className="flex-div">
+                                                            <h2>Username:</h2>
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <input id="textUsername" type="text" value={this.state.username} onChange={e => this.setState({ username: e.target.value })}></input>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <div className="flex-div">
+                                                            <h2>Password:</h2>
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <input id="textPassword" type="password" value={this.state.password} onChange={e => this.setState({ password: e.target.value })}></input>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <div className="flex-div">
+                                                            <h2>Full Name:</h2>
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <input id="textFullname" type="text" value={this.state.fullname} onChange={e => this.setState({ fullname: e.target.value })}></input>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <div className="flex-div">
+                                                            <h2>Address:</h2>
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <input id="textAddress" type="text" value={this.state.address} onChange={e => this.setState({ address: e.target.value })}></input>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <div className="flex-div">
+                                                            <h2>Email:</h2>
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <input id="textEmail" type="text" value={this.state.email} onChange={e => this.setState({ email: e.target.value })}></input>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                            <div className="flex-div">
+                                                <input id="buttonSubmit" type="submit" value="Signup"></input>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div className="basic-div">
+                                        <form onSubmit={this.handleSubmitLogin}>
+                                            <table>
+                                                <tr>
+                                                    <td>
+                                                        <div className="flex-div">
+                                                            <h2>Username:</h2>
+                                                            <input id="textUsername" type="text" value={this.state.username} onChange={e => this.setState({ username: e.target.value })}></input>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <div className="flex-div">
+                                                            <h2>Password:</h2>
+                                                            <input id="textPassword" type="password" value={this.state.password} onChange={e => this.setState({ password: e.target.value })}></input>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <div className="flex-div">
+                                                            <input id="buttonSubmit" type="submit" value="Login"></input>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                     {this.state.response}
                 </div>
             </>
--- car-app/server.js
+++ car-app/server.js
@@ -1,23 +1,85 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
 
+//Imports the required libraries to connect to the frontend and database.
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+//Imports the cryptography functions.
+const { encrypt, decrypt } = require('./crypto');
+
+//Express and MongoDB connection information.
 const app = express();
 const port = process.env.PORT || 5000;
+const url = "mongodb+srv://eemerson:Conestoga1@clustercarapp.y2dss.mongodb.net/cardatabase?authSource=admin&retryWrites=true&w=majority";
 
+//Required to parse information sent from the frontend React.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// API calls
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+//
+//
+//ACCOUNT LOGIN FUNCTION
+//
+//
+app.post('/api/login', (req, res) => {
+
+  //Connects to the database to insert the account information.
+  MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }).then(client => {
+
+    //Gets the database from the client.
+    const db = client.db("cardatabase");
+
+    //Inserts the account information into the database.
+    db.collection("accounts").findOne({username: req.body.username}, function(err, result){
+
+      //Checks if the password are equal.
+      if(decrypt(result.password) == req.body.password){
+        
+        res.send("Passwords are equal!");
+
+      }else{
+
+        res.send("Passwords are not equal!");
+
+      }
+
+    });
+
+    //Logs the inserted content in the console.
+    console.log(req.body);
+
+  });
+
 });
 
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.username}`,
-  );
+//
+//
+//ACCOUNT SIGNUP FUNCTION
+//
+//
+app.post('/api/signup', (req, res) => {
+
+  //Encrypts the password for the body.
+  req.body.password = encrypt(req.body.password);
+
+  //Connects to the database to insert the account information.
+  MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }).then(client => {
+
+    //Gets the database from the client.
+    const db = client.db("cardatabase");
+
+    //Inserts the account information into the database.
+    db.collection("accounts").insertOne(req.body);
+
+    //Logs the inserted content in the console.
+    console.log(req.body);
+
+  });
+
+  res.send("Account has been created!");
+
 });
 
+//Starts the NodeJS server.
 app.listen(port, () => console.log(`Listening on port ${port}`));
