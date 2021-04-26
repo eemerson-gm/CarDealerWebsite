0700a869c8198867f018d7d6b9996fd09a6136e9
kingnoskcaj <markjackson2112@gmail.com>
2021-04-25 20:06:04 -0400

    updated api schema

--- car-app/api/models/car.js
+++ car-app/api/models/car.js
@@ -19,6 +19,22 @@ var carSchema = new Schema({
     type: String,
     required: false
   },
+  title: {
+    type: String,
+    required: 'the title for the listing is required'
+  },
+  description: {
+    type: String,
+    required: false
+  }, 
+  condition: {
+    type: String,
+    required: false
+  }, 
+  price: {
+    type: Number,
+    required: false
+  },
   make: {
     type: String,
     required: 'the car make is required'
@@ -27,9 +43,25 @@ var carSchema = new Schema({
     type: String,
     required: 'the model type is required'
   },
+  colour: {
+    type: String,
+    required: false
+  },
+  notes: {
+    type: String,
+    required: false
+  },
+  contact: {
+    type: String,
+    required: 'the contact info is required'
+  },
   year:{
     type: Number,
     required: 'the year is required'
+  },
+  km:{
+    type: Number,
+    required: 'the number of kilometres is required'
   },
   options: [{
       type: String,
