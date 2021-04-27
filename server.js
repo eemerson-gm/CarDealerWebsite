08b221a2a2a17377e9b7fc0e8ca07a8c99ba009c
Eric Emerson <EricEmerson@users.noreply.github.com>
2021-04-27 15:33:19 -0400

    Serving react app from node server

--- server.js
+++ server.js
@@ -95,3 +95,14 @@ cars(app);
 
 //Starts the NodeJS server.
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
